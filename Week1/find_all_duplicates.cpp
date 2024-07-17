1de2bf5e9cdd3a3d65ba1c22f2a4c5cdc235fec9
pakhare <prasadkhare261@gmail.com>
2024-07-18 00:07:09 +0530

    Day 3 of Week 1

---
+++ Week1/find_all_duplicates.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> v;
+        for(int i=0; i<=nums.size()-1; i++){
+            int index = abs(nums[i])-1;
+            if(nums[index]<0)
+                v.push_back(index+1);
+            else
+                nums[index]=-nums[index];
+        }
+        return v;
+    }
+};
